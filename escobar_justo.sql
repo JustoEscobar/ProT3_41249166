9a95ce138fc279bab15e44d2babe3d8684fc841a
justoescobar <justoescobar3@gmail.com>
2024-07-21 12:33:36 -0300

    agrego base de datos

--- escobar_justo.sql
+++ escobar_justo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:3308
--- Tiempo de generación: 19-07-2024 a las 21:42:54
+-- Tiempo de generación: 21-07-2024 a las 17:32:18
 -- Versión del servidor: 10.4.27-MariaDB
 -- Versión de PHP: 8.1.12
 
@@ -63,11 +63,11 @@ CREATE TABLE `usuario` (
 
 INSERT INTO `usuario` (`id_usuario`, `nombre`, `apellido`, `usuario`, `email`, `password`, `perfil_id`, `baja`) VALUES
 (1, 'Justo', 'Escobar', 'justo98', 'justoescobar3@gmail.com', '$2y$10$O5kZyRhtbP0Q2KE4wkMd2.xQxHZZ2WT2/B3EciFVgLYhRj0XgIAea', 1, 'NO'),
-(2, 'Maria Elena', 'Soto', 'mari74', 'mari74@gmail.com', '$2y$10$133xuvAhL3k7hvBnYNV.4OeaYjJoDD3dnpIZVNvhmGFMSPU6tHnFy', 2, 'SI'),
+(2, 'Maria Elena', 'Soto', 'mari74', 'mari74@gmail.com', '$2y$10$133xuvAhL3k7hvBnYNV.4OeaYjJoDD3dnpIZVNvhmGFMSPU6tHnFy', 2, 'NO'),
 (3, 'Mateo', 'Penzo', 'mateo04', 'mateo@gmail.com', '$2y$10$Xi.ze3zBHzeU1f4CmTTI2e/edyA2abAex1S/BfQXeVfw.rDKbJQXK', 2, 'NO'),
-(4, 'Gustavo', 'Sananez', 'gustavo1975', 'gustavo@gmail.com', '$2y$10$OPmDEC4AEAWDLDTZsByNpOpg4v4V4krI3AZlzItYjMY8bQSB/5VJy', 2, 'NO'),
-(5, 'Melissa', 'Gimenez', 'melissa96', 'meli@gmail.com', '$2y$10$Txx6RKYm2UOMUC5fNolQ8.e8GC6btFS/p45ycy44SH3QahZNbWbNe', 2, 'NO'),
-(6, 'Pablo', 'Gomez', 'Pablito2597', 'pablo@gmail.com', '$2y$10$O0Wc36XtZwMvMN/8MmmyneM5VWYDfzO0tBJ/tmcajJdnlDXlzaaVG', 2, 'NO'),
+(4, 'Gustavo', 'Sananez', 'gustavo1975', 'gustavo@gmail.com', '$2y$10$OPmDEC4AEAWDLDTZsByNpOpg4v4V4krI3AZlzItYjMY8bQSB/5VJy', 2, 'SI'),
+(5, 'Melissa', 'Gimenez', 'melissa96', 'melissa@gmail.com', '$2y$10$Txx6RKYm2UOMUC5fNolQ8.e8GC6btFS/p45ycy44SH3QahZNbWbNe', 2, 'NO'),
+(6, 'Pablo Emanuel', 'Gomez', 'Pablito2597', 'pablo@gmail.com', '$2y$10$O0Wc36XtZwMvMN/8MmmyneM5VWYDfzO0tBJ/tmcajJdnlDXlzaaVG', 2, 'NO'),
 (7, 'Belen', 'Gonzalez', 'belen', 'beleng@gmail.com', '$2y$10$GdyqwS43.3QH7Qd4PXQgEeJal0MpNipOslwl.u1zNM7SVKobYz7uy', 2, 'NO'),
 (8, 'Cristian', 'Espinoza', 'cris7', 'cris@yahoo.com.ar', '$2y$10$zYXWcACBUUXZtb6So.fjeePbxOU8vgOzUaRQeL8f7dnZA7cGFTyNO', 2, 'NO'),
 (9, 'Roman', 'Alegre', 'Roman10', 'roman@gmail.com', '$2y$10$rFk7tAlntIYDXav43YuANOV2BOGoGnnGukOdrxO3QyPl6M9An7WJC', 2, 'NO'),
@@ -77,7 +77,9 @@ INSERT INTO `usuario` (`id_usuario`, `nombre`, `apellido`, `usuario`, `email`, `
 (13, 'Juan Maria', 'Gaona', 'Juan1997', 'juanmaria@gmail.com', '$2y$10$15HWd4Zs3wzXJns3F2Kz5.0Kva5Y93VLp/h0/cnlmN7LglVNQbzou', 2, 'NO'),
 (14, 'Karina', 'Gutierrez', 'Kari', 'karina@gmail.com', '$2y$10$wyk.ErX3ERg0tjMEngMUAeqxkl7c9H1xc8C3gQ7fiLU9Nv5.i5Chq', 2, 'NO'),
 (15, 'Ana Maria', 'Sanchez', 'Ana123', 'ana@gmail.com', '$2y$10$9tT7eDXNCm.TmnSVolTXIuOYcZ8cuZyeLyb2FUIVzYK4bNzjNVtd.', 2, 'NO'),
-(16, 'Perla', 'Veron ', 'Perla1940', 'perla@gmail.com', '$2y$10$mU8GkNYadn4PjfEJadM2OeCv1iFTe.lnIYk5tIebnYlAGHG5woGDa', 2, 'NO');
+(16, 'Perla', 'Veron ', 'Perla1940', 'perla@gmail.com', '$2y$10$mU8GkNYadn4PjfEJadM2OeCv1iFTe.lnIYk5tIebnYlAGHG5woGDa', 2, 'SI'),
+(17, 'Rodrigo', 'Zalazar', 'Rodrigo10', 'rodrigo@gmail.com', '$2y$10$TYXsAQsDji75.5WXS.AGl.lxV2oggBTqMkUIr9XAP5R5J9VpctPX6', 1, 'NO'),
+(18, 'Carolina', 'Comaschi', 'Caro17', 'carocomaschi@gmail.com', '$2y$10$FHNkDrrwDZPQABNHXN5IFeYCCN7A6di/S6qOJCHYtMoNuCpXH8qc.', 2, 'NO');
 
 --
 -- Índices para tablas volcadas
@@ -109,7 +111,7 @@ ALTER TABLE `perfil`
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
